7314bdb90bc156308bd3997157a7a4aa10242c6e
shouma237 <jonatha.mariano15@gmail.com>
2023-11-17 18:20:32 +0800

    lastcommit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,16 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-
 const galleryList = document.querySelector(".gallery");
 
 const createGallery = (el) => {
   return el
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
-  </li>`;
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>`;
     })
     .join("");
 };
@@ -29,37 +21,9 @@ galleryList.insertAdjacentHTML("beforeend", photos);
 
 // -------------------------------------------
 
-const handleGalleryClick = (event) => {
-  event.preventDefault();
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
-  if (event.target.nodename !== "IMG") {
-    return;
-  }
-
-  const urlOriginal = event.target.dateset.source;
-
-  // Create Baisc Lightbox Instance
-
-  const instance = basicLightbox.create(`<img src="${urlOriginal}">`);
-
-  instance.show();
-
-  // escKeyPress
-
-  const escKeyPress = (event) => {
-    if (event.key === "Escape") {
-      instance.close();
-      window.removeEventListener("keydown", escKeyPress);
-    }
-  };
-
-  // Initialize SimpleLightbox
-  const lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-
-  window.addEventListener("keydown", escKeyPress);
-};
-
-galleryList.addEventListener("click", handleGalleryClick);
+lightbox.on("show.simplelightbox");
